1d8d2e20089ba34aaec91d28044e2336c0e10890
maimand <42661871+maimand@users.noreply.github.com>
2022-06-06 09:11:21 +0700

    Revert "Ec 68 merchant integrate api products category management UI product management"

--- src/core/constants.js
+++
@@ -1,9 +0,0 @@
-export const USER_ROLE = {
-  MERCHANT: "ROLE_MERCHANT",
-  CUSTOMER: "ROLE_CUSTOMER"
-};
-
-export const ERRORS = {
-  ACCOUNT_PERMISSION: "Account don't have permission",
-  NETWORK: "Network Error"
-};
--- src/lang/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * lang.js
- */
-import en from "./en";
-import vi from "./vi";
-
-const data = Object.assign(en, vi, {});
-
-export { data };
